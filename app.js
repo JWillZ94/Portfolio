009f39553da09461a2b6da55ad2071c15a299b08
Jerel <jerelwilliams94@aol.com>
2017-10-19 00:09:45 -0400

    added more things that were missing

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render(index);
+});
+
+app.listen(3000);
