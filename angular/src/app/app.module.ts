99ade135a60d061744a7b5bb9cfb296d050f5dbe
Jerel <jerelwilliams94@aol.com>
2017-11-04 01:23:41 -0400

    added angular, changed the front-end layout, displays pages in angular style, working on design

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+// const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser({extended: false}));
+
+// app.use(cors());  Enables cross-origin resource sharing middleware to be used
 
 app.use(express.static(path.join(__dirname, 'public')));
 
