c59d3828c436528c02130db97b8cebe0e71f844a
Jerel <jerelwilliams94@aol.com>
2017-12-30 12:36:53 -0500

    Made project smaller by taking out unneeded angular

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-// const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser({extended: false}));
 
-// app.use(cors());  Enables cross-origin resource sharing middleware to be used
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render(index);
+  res.render('index');
 });
 
 app.listen(3000, () => {
-  console.log('you good fam');
+  console.log('server running');
 });
