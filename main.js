d01854e339623fcef3838bfeed8226d5ca174761
Jerel <jerelwilliams94@aol.com>
2018-05-09 16:20:41 -0400

    added more responsiveness

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 // JavaScript ====================================
-let email = document.getElementById('email');
-
-email.addEventListener('mouseover', pointer);
-
-function pointer() {
-  email.style.cursor = 'pointer';
-}
-
-(function() {
-  'use strict';
-
-  window.addEventListener('load', function() {
-    var form = document.getElementById('needs-validation');
-    form.addEventListener('submit', function(event) {
-      if (form.checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-      form.classList.add('was-validated');
-    }, false);
-  }, false);
-})();
+// let email = document.getElementById('email');
+//
+// email.addEventListener('mouseover', pointer);
+//
+// function pointer() {
+//   email.style.cursor = 'pointer';
+// }
+//
+// (function() {
+//   'use strict';
+//
+//   window.addEventListener('load', function() {
+//     var form = document.getElementById('needs-validation');
+//     form.addEventListener('submit', function(event) {
+//       if (form.checkValidity() === false) {
+//         event.preventDefault();
+//         event.stopPropagation();
+//       }
+//       form.classList.add('was-validated');
+//     }, false);
+//   }, false);
+// })();
 
 // jQuery ========================================
 $("document").ready(function() {
