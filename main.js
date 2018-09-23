b8c69c7dbd869f2ee8c3b448cd28a0c782de0b20
Jerel <jerelwilliams94@aol.com>
2018-09-23 17:51:25 -0400

    optimization

--- main.js
+++ main.js
@@ -163,3 +163,28 @@ $("document").ready(function() {
   );
 
 });
+
+// Lazy Loading ================================
+
+document.addEventListener("DOMContentLoaded", function() {
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(lazyImage => {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    console.log("This browser is not compatible with Intersection Observer.");
+  }
+});
